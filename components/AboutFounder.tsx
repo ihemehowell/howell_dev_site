2e30aa6d2638b74c048034577df65125e66f767a
Howell <kelvinuc111@gmail.com>
2026-02-07 19:12:13 +0100

    Howell Dev with all Pages Set

--- components/AboutFounder.tsx
+++ components/AboutFounder.tsx
@@ -1,9 +1,9 @@
-import howell from '../images/howell.jpg'
+
 
 
 export default function AboutFounder() {
     return (
-        <section className="py-28 px-6 border-t border-white/5">
+        <section className="py-18 px-6 border-t border-white/5">
             <div className="max-w-6xl mx-auto grid md:grid-cols-2 gap-16 items-center">
 
                 {/* Text */}
@@ -29,15 +29,13 @@ export default function AboutFounder() {
 
                 {/* Image Placeholder */}
                 <div className="glow-card w-full rounded-2xl flex items-center justify-center">
-                    <img
-                        src={howell.src}
-                        alt="Howell's Photo"
-                        width={300}
-                        
-                        className="rounded-3xl object-cover"
-                    />
+                <img
+                    src="/images/howell.jpg" 
+                    alt="Howell's Photo"
+                    width={300}
+                    className="rounded-3xl object-cover"
+                />
                 </div>
-
             </div>
         </section>
     )
